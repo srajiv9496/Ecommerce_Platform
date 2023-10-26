de893cd6019aedae7fe2817aa1e13d56502caf5e
srajiv9496 <srajiv9496@gmail.com>
2023-10-26 12:22:34 +0530

    Authentication for admin and vendor to avoid conflict

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -30,9 +30,9 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         if($request->user()->role == 'admin'){
-            return redirect()->intendddddddddddddddddddddddepppppppppppppppppppppppppppppppppppp('/admin/dashboard'::HOME);
+            return redirect()->intended('/admin/dashboard'::HOME);
         }elseif($request->user()->role == 'vendor'){
-            return redirect()->route('/vendor/dashboard'::HOME);
+            return redirect()->intended('/vendor/dashboard'::HOME);
         }
 
         return redirect()->intended(RouteServiceProvider::HOME);
