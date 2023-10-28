f3caf285b7df373c341c65d0712648799bdce006
srajiv9496 <srajiv9496@gmail.com>
2023-10-28 23:38:52 +0530

    Admin config

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,10 @@
 /** Admin Routes */
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+/** Profile Routes */
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
