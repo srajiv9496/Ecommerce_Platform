a41e705a4342a6a73f97a1cf221132d8c8eda5a9
srajiv9496 <srajiv9496@gmail.com>
2023-10-29 01:35:01 +0530

    Admin panel setup and mastering

--- app/Http/Controllers/Backend/ProfileController.php
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -4,10 +4,64 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use File;
 
 class ProfileController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('admin.profile.index');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:100'],
+            'email' => ['required', 'email', 'unique:users,email,'.Auth::user()->id],
+            'image' => ['image', 'max:2048']
+        ]);
+
+        $user = Auth::user();
+
+        if($request->hasFile('image')){
+            //checking for the profile pic, whether it's already present , if yes then update else delete.
+            if(File::exists(public_path($user->image))){
+                File::delete(public_path($user->image));
+            }
+            //uplaoding the profile pic
+            $image = $request->image;
+            $imageName = rand().'_'.$image->getClientOriginalName();
+            $image->move(public_path('uploads'),$imageName);
+
+            $path = "/uploads/".$imageName;
+
+            $user->image=$path;
+
+        }
+
+        
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+//adding toastr as an inbult prompt showing the status of the job.
+        toastr()->success('Profile Updated Successfully!');
+
+        return redirect()->back();
+    }
+
+//password validation
+    public function updatePassword(Request $request)
+    {
+        $request->validate([
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', 'min:8', 'confirmed']
+        ]);
+
+        $request->user()->update([
+            'password' => bcrypt($request->password)
+        ]);
+        toastr()->success('Profile Password Updated Successfully!');
+        return redirect()->back();
     }
 }
--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -15,31 +15,73 @@
         
         <div class="col-12 col-md-12 col-lg-7">
           <div class="card">
-            <form method="post" class="needs-validation" novalidate="">
-              <div class="card-header">
-                <h4>Edit Profile</h4>
-              </div>
-              <div class="card-body">
-                  <div class="row">                               
-                    <div class="form-group col-md-6 col-12">
-                      <label>First Name</label>
-                      <input type="text" class="form-control" value="Ujang" required="">
-                         </div>
-                    <div class="form-group col-md-6 col-12">
-                      <label>Last Name</label>
-                      <input type="text" class="form-control" value="Maman" required="">
-                      <div class="invalid-feedback">
-                        Please fill in the last name
-                      </div>
+            <form method="post" class="needs-validation" novalidate="" action="{{route('admin.profile.update')}}" enctype="multipart/form-data">
+                @csrf
+                <div class="card-header">
+                    <h4>Update Profile</h4>
+                </div>
+                <div class="card-body">
+                    <div class="row"> 
+                        
+                        <div class="form-group col-12">
+                            <div class="mb-3">
+                                <img width="100px" src="{{asset(Auth::user()->image)}}" alt="">
+                            </div>
+                            <label>Image</label>
+                            <input type="file" name="image" class="form-control" value="{{Auth::user()->name}}">
+                        </div>
+
+                        <div class="form-group col-md-6 col-12">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control" value="{{Auth::user()->name}}">
+                        </div>
+                        <div class="form-group col-md-6 col-12">
+                            <label>Email</label>
+                            <input type="email" name="email" class="form-control" value="{{Auth::user()->email}}">
+                        </div>
                     </div>
-                  </div
-              </div>
-              <div class="card-footer text-right">
-                <button class="btn btn-primary">Save Changes</button>
-              </div>
+                </div>
+
+                <div class="card-footer text-right">
+                    <button class="btn btn-primary">Save Changes</button>
+                </div>
             </form>
           </div>
         </div>
+
+        
+        <div class="col-12 col-md-12 col-lg-7">
+            <div class="card">
+              <form method="post" class="needs-validation" novalidate="" action="{{route('admin.password.update')}}" enctype="multipart/form-data">
+                  @csrf
+                  <div class="card-header">
+                      <h4>Update Password</h4>
+                  </div>
+                  <div class="card-body">
+                      <div class="row"> 
+                         
+                          <div class="form-group  col-12">
+                              <label>Current Password</label>
+                              <input type="password" name="current_password" class="form-control">
+                          </div>
+                          <div class="form-group  col-12">
+                              <label>New Password</label>
+                              <input type="password" name="password" class="form-control">
+                          </div>
+                          <div class="form-group  col-12">
+                              <label>Confirm Password</label>
+                              <input type="password" name="password_confirmation" class="form-control">
+                          </div>
+                      </div>
+                  </div>
+  
+                  <div class="card-footer text-right">
+                      <button class="btn btn-primary">Save Changes</button>
+                  </div>
+              </form>
+            </div>
+          </div>
+
       </div>
     </div>
   </section>
