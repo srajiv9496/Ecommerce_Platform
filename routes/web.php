450f3a87104c7ac6b2e1d9c62402360860d633ab
srajiv9496 <srajiv9496@gmail.com>
2023-11-02 23:58:14 +0530

    User Dashboard creation and setup

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,9 +20,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('frontend.Dashboard.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -32,3 +30,8 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
+
+
+Route::group(['middleware'=>['auth', 'verified'], 'prefix'=> 'user', 'as' => 'user.'], function(){
+    Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+});
