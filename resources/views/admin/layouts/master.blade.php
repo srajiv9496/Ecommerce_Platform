3108d392fda10f0de8baa4a8476ee518a4fed970
srajiv9496 <srajiv9496@gmail.com>
2023-11-07 23:25:24 +0530

    mastering the admin panel

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -47,7 +47,7 @@
       </div>
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="#">Rajiv Kumar Singh</a>
         </div>
         <div class="footer-right">
           
